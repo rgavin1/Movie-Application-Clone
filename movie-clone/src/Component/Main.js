07e045fcf4ab05a88e1b5661865c057438e28e23
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2021-03-14 16:25:49 -0400

    Clean Out

---
+++ movie-clone/src/Component/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Assets/styles/Main.css';
+
+const Main = () => {
+    return  <div className="main">
+                <h1>Main</h1>
+                Image
+                Suggestions
+            </div>;
+} 
+
+export default Main;
---
+++ movie-clone/src/Component/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Assets/styles/Sidebar.css';
+
+const Sidebar = () => {
+    return  <div className="sidebar">
+                <h1>Sidebar</h1>
+                home 
+                search
+                tv
+                movie
+            </div>;
+}
+
+export default Sidebar;
