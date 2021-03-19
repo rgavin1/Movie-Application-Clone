c9fe23ccc60ecf90bbb0499eb3217b8646a850b8
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2021-03-18 20:16:09 -0400

    installed framer motion

--- movie-clone/src/App.js
+++ movie-clone/src/App.js
@@ -9,6 +9,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Component } from 'react';
+
+import { motion } from "framer-motion"
+
 require('dotenv').config();
 
 
