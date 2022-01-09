6577429d6f673f0f4bec86577efafeeb2cc860cd
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-01-08 22:14:49 -0700

    Update env file

--- movie-clone/src/Component/Pages/Tv.js
+++ movie-clone/src/Component/Pages/Tv.js
@@ -15,7 +15,7 @@ const Tv = () => {
 
     useEffect(() => {
         const fetchPopShows = () => {
-            fetch(`https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_TMDB}`)
+            fetch(`https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             .then(res => res.json())
             .then((items) => {
                 const random_num = Math.floor(Math.random() * 19);
@@ -33,7 +33,7 @@ const Tv = () => {
         }
     
         const fetchRelatedShows = ({ tv_id = 88396 }) => {
-            fetch(`https://api.themoviedb.org/3/tv/${tv_id}/similar?api_key=${process.env.REACT_APP_TMDB}&language=en-US&page=1`)
+            fetch(`https://api.themoviedb.org/3/tv/${tv_id}/similar?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`)
             .then(res => res.json())
             .then((items) => {
                 setRelatedshows(items.results);
@@ -44,7 +44,7 @@ const Tv = () => {
         }
     
         const fetchRecommendedShows = ({ tv_id = 88396 }) => {
-            fetch(`https://api.themoviedb.org/3/tv/${tv_id}/recommendations?api_key=${process.env.REACT_APP_TMDB}&language=en-US&page=1`)
+            fetch(`https://api.themoviedb.org/3/tv/${tv_id}/recommendations?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`)
             .then(res => res.json())
             .then((items) => {
                 setRecommendedshows(items.results);
@@ -59,7 +59,7 @@ const Tv = () => {
    
 
     function fetchTopRatedShows () {
-        fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.REACT_APP_TMDB}&language=en-US&page=1`)
+        fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`)
         .then(res => res.json())
         .then((items) => {
             setTopRatedShows(items.results);
