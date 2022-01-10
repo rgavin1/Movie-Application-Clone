a03ca6d8431fbea715ba8c423a9aa464f45bb253
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-01-09 18:51:49 -0700

    Convert App to Hooks

--- movie-clone/src/App.js
+++ movie-clone/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Main from './pages/home/Container';
@@ -11,6 +11,57 @@ import "slick-carousel/slick/slick-theme.css";
 import routes from "./utils/routes";
 
 require('dotenv').config();
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       feature: {},
+//       trending_all: [],
+//       searching: false
+//     }
+//   }
+
+//   componentDidMount() {
+//       fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
+//       .then(res => res.json())
+//       .then((items) => {
+//         const random_num = Math.floor(Math.random() * 19);
+//         this.setState({
+//           feature: items.results[random_num],
+//           trending_all: items.results,
+//           searching: false
+//         },
+//         (error) => {
+//           this.setState({
+//             searching: true,
+//             error
+//           });
+//         } 
+//         )
+//       });
+//   }
+//   render(){
+    
+//     return (
+//       <Router>
+//       <div className="app">
+//           <SideBar />
+//           <Switch>
+//             {/* FIXME: Add with the rest of routes */}
+//               <Route exact path="/app"> 
+//                 <Main feature={this.state.feature} trending={this.state.trending_all} />
+//             </Route>
+//             {
+//               routes.map((route, index) => {
+//                 return <Route key={index} path={route.pathname} component={route.component} /> 
+//               })
+//             }
+//           </Switch>
+//       </div>  
+//       </Router>
+//     );
+//   }
+// }
 
 class App extends Component {
   constructor(props){
@@ -23,23 +74,34 @@ class App extends Component {
   }
 
   componentDidMount() {
-      fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
-      .then(res => res.json())
-      .then((items) => {
+
+    (async () => {
+
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
+
+        const items = await response.json();
+       
         const random_num = Math.floor(Math.random() * 19);
         this.setState({
           feature: items.results[random_num],
           trending_all: items.results,
           searching: false
-        },
-        (error) => {
-          this.setState({
+        });
+          
+      } catch (e) {
+        
+        this.setState({
+            e
+        });
+        
+      } finally {
+                
+        this.setState({
             searching: true,
-            error
-          });
-        } 
-        )
-      });
+        });
+      }
+          })()
   }
   render(){
     
