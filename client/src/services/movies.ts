63c51608ad765b4fa140b84f7d207a78342cf974
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-09-03 23:02:40 -0700

    oops

--- client/src/services/movies.ts
+++ client/src/services/movies.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { RawResponse } from '../utils/types'
 import { trendingMoviesResponse } from './mocks/trendingMovies'
 
-const BASE_URL = "http://localhost:8000/api/"
+const BASE_URL = "http://localhost:8000/api/movies"
 
 // TODO: Clean-up
 const getTrendingMoviesForWeek = (): RawResponse => {
@@ -10,22 +10,22 @@ const getTrendingMoviesForWeek = (): RawResponse => {
 }
 
 const fetchPopularMovies = async (): Promise<RawResponse> => {
-    const { data } = await axios.get(`${BASE_URL}/movie/popular`)
+    const { data } = await axios.get(`${BASE_URL}/popular`)
     return data
 }
 
 const fetchTopRated = async (): Promise<RawResponse> => {
-    const { data } = await axios.get(`${BASE_URL}/movie/top-rated`)
+    const { data } = await axios.get(`${BASE_URL}/top-rated`)
     return data
 }
 
 const fetchNowPlaying = async (): Promise<RawResponse> => {
-    const { data } = await axios.get(`${BASE_URL}/movie/now-playing`)
+    const { data } = await axios.get(`${BASE_URL}/now-playing`)
     return data
 }
 
 const fetchUpcomingMovies = async (): Promise<RawResponse> => {
-    const { data } = await axios.get(`${BASE_URL}/movie/upcoming`)
+    const { data } = await axios.get(`${BASE_URL}/upcoming`)
     return data
 }
 
