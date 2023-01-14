0107a8634cab70243183b2e65e8f478d726b6374
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2023-01-13 21:17:19 -0700

    checkpoint: feature is now working

--- client/src/services/feature.ts
+++ client/src/services/feature.ts
@@ -12,7 +12,7 @@ const getTrending = async (): Promise<Program[]> => {
 
 const getFeature = async (media: Omit<MediaType, "person">): Promise<RawResponse> => {
     const { data } = await axiosInstance.get("/features", { params: { media } })
-    return data
+    return data.data
 }
 
 const featureService = {
--- server/src/api/routes/features.ts
+++ server/src/api/routes/features.ts
@@ -1,18 +1,16 @@
 import { AxiosError } from 'axios';
-import express, { Request, Response } from 'express'
+import express, { Request, Response, Router } from 'express'
 import { axiosInstance } from '../../service/tmdb'
 
-const router = express.Router()
-router.get('/', async (req: Request, res: Response) => {
-    const url = `trending/${req.query.media}/weeka`
-
+const router: Router = express.Router()
+router.get('/', async (req: Request, res: Response): Promise<void> => {
+    const url = `trending/${req.query.media}/week`
     try {
         const { data } = await axiosInstance.get(url)
         res.json({ "status": "success", data })
     } catch (error: AxiosError | any) {
-        if (error.message) {
-            res.json({ "status": 400, "message": error.message })
-        } else {
+        if (error.message) res.json({ "status": 400, "message": error.message })
+        else {
             res.json({ "message": error.message })
         }
     }
