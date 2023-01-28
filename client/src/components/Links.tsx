87d2efc1606d5f15620456bb7dd208a55770328d
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2023-01-27 23:50:33 -0700

    checkpoint: simplifying the code

--- client/src/components/Links.tsx
+++ client/src/components/Links.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { faLink } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faImdb, faFacebook, faInstagram, faWikipediaW } from '@fortawesome/free-brands-svg-icons'
 import axios from 'axios';
 
 // https://www.imdb.com/title/tt0434665/
@@ -9,16 +10,36 @@ import axios from 'axios';
 // https://www.wikidata.org/wiki/Q105553568
 // https://www.facebook.com/netflixvalhalla/
 
-const ExternalBaseUrls = {
-    imdb_id: "https://www.imdb.com/title/",
+const ExternalLinks: { [key: string]: any } = {
+    imdb_id: {
+        title: "IMDB",
+        url: "https://www.imdb.com/title/",
+        icon: faImdb
+    },
     freebase_mid: null,
     freebase_id: null,
     tvdb_id: null,
     tvrage_id: null,
-    facebook_id: "https://www.facebook.com/",
-    instagram_id: "https://www.instagram.com/",
-    twitter_id: "https://twitter.com/",
-    wikidata_id: "https://www.wikidata.org/wiki/"
+    facebook_id: {
+        title: "Facebook",
+        url: "https://www.facebook.com/",
+        icon: faFacebook
+    },
+    instagram_id: {
+        title: "Instagram",
+        url: "https://www.instagram.com/",
+        icon: faInstagram
+    },
+    twitter_id: {
+        title: "Twitter",
+        url: "https://twitter.com/",
+        icon: faTwitter
+    },
+    wikidata_id: {
+        title: "Wiki Data",
+        url: "https://www.wikidata.org/wiki/",
+        icon: faWikipediaW
+    }
 }
 
 type ExternalIds = {
@@ -41,10 +62,34 @@ const baseExternalIds: ExternalIds = {
     tvdb_id: null,
     tvrage_id: null,
     facebook_id: null,
-    instagram_id: "",
+    instagram_id: null,
     twitter_id: null,
     id: null,
     wikidata_id: null
+}
+
+const socialMediaLink = (linkName: string, linkId: string): JSX.Element => {
+    const { title, url, icon } = ExternalLinks[linkName]
+
+    return <li title={title}><a href={url + linkId} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={icon as any} /></a></li>
+}
+
+const ListOfExternalLinks = (externalIds: ExternalIds): JSX.Element[] => {
+    const { imdb_id, facebook_id, instagram_id, twitter_id, wikidata_id } = externalIds
+
+    let Links = []
+
+    if (imdb_id) Links.push(socialMediaLink("imdb_id", imdb_id))
+
+    if (facebook_id) Links.push(socialMediaLink("facebook_id", facebook_id))
+
+    if (instagram_id) Links.push(socialMediaLink("instagram_id", instagram_id))
+
+    if (twitter_id) Links.push(socialMediaLink("twitter_id", twitter_id))
+
+    if (wikidata_id) Links.push(socialMediaLink("wikidata_id", wikidata_id))
+
+    return Links
 }
 
 const Links: React.FC<{ item: any }> = ({ item }) => {
@@ -61,15 +106,11 @@ const Links: React.FC<{ item: any }> = ({ item }) => {
         })()
     }, [item])
 
-    console.log('externalIds', externalIds)
-    console.log('ExternalBaseUrls', ExternalBaseUrls["instagram_id"])
     return <>
         <h1>Links</h1>
         <ul className="information__links">
             <li title="homepage"><a href={item?.homepage} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={faLink} /></a></li>
-            <li title="imdb"><a href={"https://www.imdb.com/title/" + externalIds?.imdb_id + "/"} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={faLink} /></a></li>
-            <li title="instagram"><a href={"https://www.instagram.com/" + externalIds?.instagram_id + "/"} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={faLink} /></a></li>
-            <li title="twitter"><a href={"https://twitter.com/" + externalIds?.twitter_id + "/"} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={faLink} /></a></li>
+            {ListOfExternalLinks(externalIds).map((link: JSX.Element) => link)}
         </ul>
     </>
 }
